a35ecbccd73a8c7668b6733dd87c152b1de98b40
wsrwagner <wsr.wagner@gmail.com>
2025-09-03 23:57:11 +0000

    Início do projeto

--- Super_Trunfo.c
+++ Super_Trunfo.c
@@ -1,1 +1,25 @@
-git status
+#include <stdio.h>
+
+int main(){
+
+//informações da primeira carta
+char estado1[1];
+char codcarta1[3];
+char ncidade1[20];
+int população1;
+float area1;
+float pib1;
+int pontost1;
+
+//informações da segunda carta
+char estado2[1];
+char codcarta2[3];
+char ncidade2[20];
+int população2;
+float area2;
+float pib2;
+int pontost2;
+
+printf("informe os dados para primeira carta\n");
+
+}
