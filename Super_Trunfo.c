ab0db43a8cbe7f62ccb1e5954ffa76645d88b4f0
wsrwagner <wsr.wagner@gmail.com>
2025-09-04 01:38:34 +0000

    Impressão das cartas

--- Super_Trunfo.c
+++ Super_Trunfo.c
@@ -43,14 +43,14 @@ scanf(" %f", &pib1);
 
 printf("Informe a quantidade de pontos turísticos na cidade.\n");
 scanf(" %d", &pontost1);
-
+\n
 //inserção dos dados da segunda carta
 printf("Vamos continuar, agora todos os dados que irá informar a seguir serão da segunda carta e devem se tratar da mesma cidade.\n\n");
 
-printf("Informe uma letra de 'A' a 'H' representando um dos oito estados para primeira carta\n");
+printf("Informe uma letra de 'A' a 'H' representando um dos oito estados para segunda carta\n");
 scanf(" %s", &estado2);
 
-printf("Informe um número de 01 a 04 para o código da primeira carta\n");
+printf("Informe um número de 01 a 04 para o código da segunda carta\n");
 scanf(" %d", &codcarta2);
 
 printf("Informe o nome da cidade.\n");
@@ -65,10 +65,29 @@ scanf(" %f", &area2);
 printf("Informe o Produto Interno Bruto da cidade.\n");
 scanf(" %f", &pib2);
 
-printf("Informe a quantidade de pontos turísticos na cidade.\n");
+printf("Informe a quantidade de pontos turísticos na cidade.\n\n");
 scanf(" %d", &pontost2);
 
+//demonstração da carta 1 
 
+printf("Carta 1\n");
+printf("Estado: %s\n", estado1);
+printf("Código: %d\n", estado1, codcarta1);
+printf("Cidade: %s\n", cidade1);
+printf("População: %d\n", população1);
+printf("Área: %f\n", area1);
+printf("PIB: %f\n", pib1);
+printf("Pontos turísticos: %d\n\n", pontost1);
 
+//demonstração da carta 2 
+
+printf("Carta 2\n");
+printf("Estado: %s\n", estado2);
+printf("Estado %s | Código: %d\n", estado2, codcarta2);
+printf("Cidade: %s\n", cidade2);
+printf("População: %d\n", população2);
+printf("Área: %f\n", area2);
+printf("PIB: %f\n", pib2);
+printf("Pontos turísticos: %d\n\n", pontost2);
 
 }
