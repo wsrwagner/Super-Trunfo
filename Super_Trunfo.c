b5c43cbf81e4833851e49fdc93ef85e5c53c97a7
wsrwagner <wsr.wagner@gmail.com>
2025-09-04 00:32:09 +0000

    Adicionados parâmetros para inserir os dados das cartas

--- Super_Trunfo.c
+++ Super_Trunfo.c
@@ -2,24 +2,73 @@
 
 int main(){
 
-//informações da primeira carta
+//variaveis da primeira carta
 char estado1[1];
-char codcarta1[3];
-char ncidade1[20];
+int codcarta1[2];
+char cidade1[20];
 int população1;
 float area1;
 float pib1;
 int pontost1;
 
-//informações da segunda carta
+//variaveis da segunda carta
 char estado2[1];
-char codcarta2[3];
-char ncidade2[20];
+int codcarta2[2];
+char cidade2[20];
 int população2;
 float area2;
 float pib2;
 int pontost2;
 
-printf("informe os dados para primeira carta\n");
+//inserção dos dados da primeira carta
+printf("Vamos começar, todos os dados que irá informar a seguir serão da primeira carta e devem se tratar da mesma cidade.\n\n");
+
+printf("Informe uma letra de 'A' a 'H' representando um dos oito estados para primeira carta\n");
+scanf(" %s", &estado1);
+
+printf("Informe um número de 01 a 04 para o código da primeira carta\n");
+scanf(" %d", &codcarta1);
+
+printf("Informe o nome da cidade.\n");
+scanf(" %s", &cidade1);
+
+printf("Informe o número de habitantes da cidade.\n");
+scanf(" %d", &população1);
+
+printf("Informe a área da cidade em quilômetros quadrados da cidade\n");
+scanf(" %f", &area1);
+
+printf("Informe o Produto Interno Bruto da cidade.\n");
+scanf(" %f", &pib1);
+
+printf("Informe a quantidade de pontos turísticos na cidade.\n");
+scanf(" %d", &pontost1);
+
+//inserção dos dados da segunda carta
+printf("Vamos continuar, agora todos os dados que irá informar a seguir serão da segunda carta e devem se tratar da mesma cidade.\n\n");
+
+printf("Informe uma letra de 'A' a 'H' representando um dos oito estados para primeira carta\n");
+scanf(" %s", &estado2);
+
+printf("Informe um número de 01 a 04 para o código da primeira carta\n");
+scanf(" %d", &codcarta2);
+
+printf("Informe o nome da cidade.\n");
+scanf(" %s", &cidade2);
+
+printf("Informe o número de habitantes da cidade.\n");
+scanf(" %d", &população2);
+
+printf("Informe a área da cidade em quilômetros quadrados da cidade\n");
+scanf(" %f", &area2);
+
+printf("Informe o Produto Interno Bruto da cidade.\n");
+scanf(" %f", &pib2);
+
+printf("Informe a quantidade de pontos turísticos na cidade.\n");
+scanf(" %d", &pontost2);
+
+
+
 
 }
